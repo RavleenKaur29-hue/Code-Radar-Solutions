2dd663d7bc22f118b3732bad18436410258ea0d8
RavleenKaur29-hue <ravleen0680.becse24@chitkara.edu.in>
2025-01-28 22:18:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -19,7 +19,7 @@ int main()
         case '/':
             if(b!=0)
             {
-            printf("%f\n",a/(float)b);
+            printf("%.0f\n",a/(float)b);
             }
             else 
             {
