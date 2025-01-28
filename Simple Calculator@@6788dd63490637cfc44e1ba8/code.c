6f7d857b149947bd6bf632664bb1c3fa7da998f5
RavleenKaur29-hue <ravleen0680.becse24@chitkara.edu.in>
2025-01-28 23:02:46 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,39 +1,37 @@
-#include <stdio.h>
-
-int main() {
-    int a, b;
+#include<stdio.h>
+int main()
+{
+    int a,b;
     char op;
-
-    // Prompt for user input
     printf("");
-    
-    // Input two integers and operator
-    if (scanf("%d %d %c", &a, &b, &op) != 3) {
-        printf("Error\n");
-        return 1;
-    }
-
-    // Perform operation based on operator
-    switch (op) {
+    scanf("%d %d %c",&a,&b,&op);
+    switch(op)
+    {
         case '+':
-            printf("%d\n", a + b);
+            printf("%d\n",a+b);
             break;
         case '-':
-            printf("%d\n", a - b);
+            printf("%d\n",a-b);
             break;
         case '*':
-            printf("%d\n", a * b);
+            printf("%d\n",a*b);
             break;
         case '/':
-            if (b != 0) {
-                printf("%.0f\n", a / (float)b);
-            } else {
-                printf("Error: Division by zero\n");
+            if(b!=0)
+            {
+            printf("%.0f\n",a/(float)b);
+            }
+            else 
+            {
+                printf("Error\n");
             }
             break;
         default:
-            printf("Error\n");
+            printf("Error\n");    
     }
-
     return 0;
 }
+ChatGPT said:
+
+ChatGPT
+
