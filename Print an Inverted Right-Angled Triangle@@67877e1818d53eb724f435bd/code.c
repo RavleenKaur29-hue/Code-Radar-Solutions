7f8491c63c6e1c422ca92ff1e24286e733b9df09
RavleenKaur29-hue <ravleen0680.becse24@chitkara.edu.in>
2025-02-27 11:38:25 +0530

    Solution to Print an Inverted Right-Angled Triangle

--- Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
+++ Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
@@ -3,55 +3,13 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    if(n==1)
+    for(i=n;i>=n;i++)
     {
-        printf("*");
-    }
-    else if(n==2)
-    {
-        printf("* *\n");
-        printf("*");
-    }
-    else if(n==3)
-    {
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-     else if(n==4)
-    {
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*\n");
-    }
-     else if(n==5)
-    {
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*\n");
-        
-    }
-     else if(n==6)
-    {
-        printf("* * * * * *\n");
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
-    }
-     else if(n==7)
-    {
-        printf("* * * * * * *\n");
-        printf("* * * * * *\n");
-        printf("* * * * *\n");
-        printf("* * * *\n");
-        printf("* * *\n");
-        printf("* *\n");
-        printf("*");
+        for(j=1;j<=n;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
     }
     return 0;
 }
